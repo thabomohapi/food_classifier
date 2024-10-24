6d153f588e3472292d6cf7e9f7ee719e781d0221
ThaboMohapiNav <Thabom@navigare.co.za>
2024-10-24 06:17:04 +0200

    can now perform inference using predictor.py

--- src/inference/predictor.py
+++ src/inference/predictor.py
@@ -2,6 +2,8 @@ import requests
 
 # Make a prediction
 url = 'http://localhost:5000/predict'
-files = {'file': open('image.jpg', 'rb')}
+files = {'file': open('data/SampleImage1.jpg', 'rb')}
 response = requests.post(url, files=files)
 predictions = response.json()
+
+print(predictions)
